45b18e79f76216ade8d54798768f98bb7f35f177
blebleli <hsj128jhs@gmail.com>
2018-09-03 01:22:26 +0900

    0903 notebook commit

--- finalProject/src/main/java/cvs/mapController.java
+++ finalProject/src/main/java/cvs/mapController.java
@@ -1,15 +1,23 @@
 package cvs;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+	
 @RequestMapping("/map")
 @Controller("mapController")
 public class mapController {
 	
 	@RequestMapping("/mapTest")
-	public String listTest(Model model) {
-		return "mapTest/map";
-	}
+	 public String boardView() {
+		
+		 return "map/maptest";
+	 }
+	
+	
+	@RequestMapping("/mapConvert")
+	 public String mapConvert() {
+		
+		 return "map/convert";
+	 }
 }
